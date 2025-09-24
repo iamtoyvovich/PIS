626cd03f38cad83571e54414df93eb01124ea172
Yevhen Zaiets <zaiets.yevhen@lll.kpi.ua>
2025-09-24 09:13:19 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  PIS
+//
+//  Created by Yevhen Zaiets on 9/24/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
